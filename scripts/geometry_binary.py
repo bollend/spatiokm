c22b5d8013575ab88e5659b7c09e98a4fa41720c
dylanb <dylanbollendb@gmail.com>
2018-06-28 12:18:21 +1000

    disk_grid added

--- scripts/geometry_binary.py
+++ scripts/geometry_binary.py
@@ -30,3 +30,32 @@ def T0_to_IC(omega, ecc, P, T0):
         return T0 - M_inf / n_inf
     else:
         return T0 - P + M_inf / n_inf
+
+def disk_grid(radius_primary, inclination, number_of_gridpoints):
+    import disk_grid_fibonacci
+    '''
+    Create a Fibonacci grid of N gridpoints to represent the
+    evolved star with a radius R, for a certain inclination.
+
+    Parameters
+    ----------
+    radius_primary : float
+        Stellar radius of the evolved star in units of the semi-major axis
+    inclination : float
+        Inclination of the orbital plane
+    number_of_gridpoints : integer
+        The number of gridpoints on the Fibonacci grid.
+
+    Returns
+    -------
+    (x,y,z) coordinates: numpy.ndarray
+        The coordinates of the gridpoints relative to the centre of the
+        evolved component in an array with dimensions (Npoints, 3)
+    '''
+    grid = disk_grid_fibonacci.disk_grid_fibonacci(number_of_gridpoints,\
+            radius_primary, [0,0])
+    grid_primary = np.array( [np.array(-grid[:,0]),\
+            np.array(-grid[:,1] * np.cos(inclination)), \
+            np.array(grid[:,1] * np.sin(inclination))] ).T
+
+    return grid_primary
