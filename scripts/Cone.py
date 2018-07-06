a19b56a5aedbf145846a7f7ed0dd8f5833d245de
dylanb <dylanbollendb@gmail.com>
2018-07-06 17:37:29 +1000

    getter setter included

--- scripts/Cone.py
+++ scripts/Cone.py
@@ -33,7 +33,7 @@ class Cone3D(object):
     ======
 
     TypeError
-        When 'centre' is not a Point.
+        When 'centre' is not a Point3D.
 
     Examples
     ========
@@ -84,5 +84,37 @@ class Cone3D(object):
             Point where the line-of-sight leaves the jet
 
         """
+        if isinstance(ray, Ray3D):
+            if not
+            #solve equation of form at**2 + bt + c = 0
+            #first determine a, b, and c
+            a = ()
+
+
         if not isintance(ray, Ray3D):
             raise TypeError('The ray should be of type Ray3D')
+
+
+    # def equation(self, x='x', y='y', z='z'):
+    #     """The equation of the cone.
+    #     Parameters
+    #     ==========
+    #     x : str, optional
+    #         Label for the x-axis. Default value is 'x'.
+    #     y : str, optional
+    #         Label for the y-axis. Default value is 'y'.
+    #     z : str, optional
+    #         Label for the y-axis. Default value is 'y'.
+    #     Returns
+    #     =======
+    #     equation : sympy expression
+    #
+    #     Examples
+    #     ========
+    #     pass
+    #     """
+    #     x = _symbol(x, real=True)
+    #     y = _symbol(y, real=True)
+    #     t1 = ((x - self.center.x) / self.hradius)**2
+    #     t2 = ((y - self.center.y) / self.vradius)**2
+    #     return t1 + t2 - 1
